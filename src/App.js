1d0eefa9a961de8a2487cbf859252c3dfd2c9092
David Wu <david204code@gmail.com>
2021-05-19 16:23:01 +0100

    Removed unused code in index.html and add style margin:0

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="app">
+      <h1>Peace to you</h1>
+      <p>top</p>
+      <p>top -intro -portfolio -work ...</p>
+      <p>sec -intro -portfolio -work ...</p>
+      <p>-intro -portfolio -work ...</p>
+      <p>-portfolio -work ...</p>
+      <p>-work ...</p>
     </div>
   );
 }
