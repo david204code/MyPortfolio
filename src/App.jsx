949f271050b11b351901d7825c1090f505ea0fec
David Wu <david204code@gmail.com>
2021-05-31 18:19:34 +0100

    directory/folder setup of assets, components & pages. And a HomePage function/.jsx file ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import "./App.css";
+import HomePage from "./pages/homePage/HomePage";
 
 function App() {
   return (
     <div className="app">
-      <h1>Peace to you</h1>
-      <p>top</p>
-      <p>top -intro -portfolio -work ...</p>
-      <p>sec -intro -portfolio -work ...</p>
-      <p>-intro -portfolio -work ...</p>
-      <p>-portfolio -work ...</p>
-      <p>-work ...</p>
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/homePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HomePage.css";
+
+export default function HomePage() {
+  return (
+    <div>
+      <p>top</p>
+      <p>top -intro -portfolio -work ...</p>
+      <p>sec -intro -portfolio -work ...</p>
+      <p>-intro -portfolio -work ...</p>
+      <p>-portfolio -work ...</p>
+      <p>-work ...</p>
+    </div>
+  );
+}
