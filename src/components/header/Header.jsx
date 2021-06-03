e2bd626a95f85ed712b9db6c9f4c50ead2c401a0
David Wu <david204code@gmail.com>
2021-06-03 18:42:25 +0100

    Using react-icons...Github/LinkedIn, styling it with IconContext.Provider and added links to Github/LinkedIn profile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Header.scss";
+import { IconContext } from "react-icons";
+import { FaGithub } from "react-icons/fa";
+import { SiLinkedin } from "react-icons/si";
 
 export default function Header() {
   return (
@@ -15,14 +18,27 @@ export default function Header() {
             <a href="#mywork">My work</a>
           </div>
         </div>
-        <div className="right">
-          <div className="rightWrapper">
-            <span>GitHub</span>
+
+        <IconContext.Provider value={{ size: "2em" }}>
+          <div className="right">
+            <div className="rightWrapper">
+              <span>
+                <a href="https://github.com/david204code">
+                  {/* <FaGithub size="2em" title="Github icon" /> */}
+                  <FaGithub title="Github icon" />
+                </a>
+              </span>
+              {/* <span>GitHub</span> */}
+            </div>
+            <div className="rightWrapper">
+              <a href="https://www.linkedin.com/in/davidwu356/">
+                {/* <SiLinkedin size="2em" color="#0050DA" title="Linkedin icon" /> */}
+                <SiLinkedin color="#0050DA" title="Linkedin icon" />
+              </a>
+              {/* <span>LinkedIn</span> */}
+            </div>
           </div>
-          <div className="rightWrapper">
-            <span>LinkedIn</span>
-          </div>
-        </div>
+        </IconContext.Provider>
       </div>
     </div>
   );
