d1d343ec0b7438b4e1f783276b75dd78ecc18807
David Wu <david204code@gmail.com>
2021-06-04 23:49:56 +0100

    Add and import new component TechStack onto the HomePage

---
+++ src/components/techStack/TechStack.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TechStack.scss";
+
+export default function TechStack() {
+  return (
+    <div>
+      <div className="techstack">
+        <p>Technology stack</p>
+      </div>
+    </div>
+  );
+}
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./HomePage.scss";
 import Intro from "../../components/intro/Intro";
+import TechStack from "../../components/techStack/TechStack";
 
 export default function HomePage() {
   return (
     <div className="homepage">
       <Intro />
+      <TechStack />
       <p>top</p>
       <p>top -intro -portfolio -work ...</p>
       <p>sec -intro -portfolio -work ...</p>
