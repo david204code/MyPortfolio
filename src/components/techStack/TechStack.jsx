b9d44c0d4480a15ee36542fabf6ad9c5129484d9
David Wu <david204code@gmail.com>
2021-06-07 23:46:47 +0100

    Created and imported TechStackList along with scss

--- src/components/techStack/TechStack.jsx
+++ src/components/techStack/TechStack.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./TechStack.scss";
+import TechStackList from "../techStackList/TechStackList";
 
 export default function TechStack() {
   return (
     <div className="techStack" id="techStack">
       <h1>My Tech Stack ... train in Full Stack</h1>
       <ul>
-        <li></li>
+        <TechStackList />
       </ul>
     </div>
   );
---
+++ src/components/techStackList/TechStackList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./TechStackList.scss";
+
+export default function TechStackList() {
+  return (
+    <li className="techStackList">
+      <h1>First name on the list</h1>
+    </li>
+  );
+}
