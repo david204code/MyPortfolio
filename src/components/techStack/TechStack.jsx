c0544c9d907cfe8eb7fb6de21d5c5a9178cc24e7
David Wu <david204code@gmail.com>
2021-06-07 16:20:44 +0100

    Add data.js and starting to work on the TechStack section

--- src/components/techStack/TechStack.jsx
+++ src/components/techStack/TechStack.jsx
@@ -3,10 +3,11 @@ import "./TechStack.scss";
 
 export default function TechStack() {
   return (
-    <div>
-      <div className="techstack">
-        <p>Technology stack</p>
-      </div>
+    <div className="techStack" id="techStack">
+      <h1>My Tech Stack, Full Stack</h1>
+      <ul>
+        <li></li>
+      </ul>
     </div>
   );
 }
---
+++ src/components/techStack/data.js
@@ -0,0 +1,56 @@
+export const frontEnd = [
+  {
+    id: 1,
+    title: "HTML",
+  },
+  {
+    id: 2,
+    title: "CSS",
+  },
+  {
+    id: 3,
+    title: "SASS",
+  },
+  {
+    id: 4,
+    title: "Bootstrap",
+  },
+  {
+    id: 5,
+    title: "JavaScript",
+  },
+  {
+    id: 6,
+    title: "React",
+  },
+];
+
+export const backEnd = [
+  {
+    id: 1,
+    title: "Ruby",
+  },
+  {
+    id: 2,
+    title: "Ruby on Rails",
+  },
+  {
+    id: 3,
+    title: "REST APIs",
+  },
+];
+
+export const others = [
+  {
+    id: 1,
+    title: "Git",
+  },
+  {
+    id: 2,
+    title: "MySQL",
+  },
+  {
+    id: 3,
+    title: "Heroku",
+  },
+];
