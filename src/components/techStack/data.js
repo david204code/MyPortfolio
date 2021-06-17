b2bb06baa5fadf5a7e57edb8a7bb6c58b08e2db1
David Wu <david204code@gmail.com>
2021-06-17 19:19:32 +0100

    Add color to data.js

--- src/components/techStack/data.js
+++ src/components/techStack/data.js
@@ -10,31 +10,37 @@ export const frontEnd = [
     id: 1,
     title: "HTML",
     image: AiFillHtml5,
+    color: "Red",
   },
   {
     id: 2,
     title: "CSS",
     image: DiCss3,
+    color: "Red",
   },
   {
     id: 3,
     title: "SASS",
     image: DiSass,
+    color: "Red",
   },
   {
     id: 4,
     title: "Bootstrap",
     image: BsBootstrapFill,
+    color: "Red",
   },
   {
     id: 5,
     title: "JavaScript",
     image: SiJavascript,
+    color: "Red",
   },
   {
     id: 6,
     title: "React",
     image: GrReactjs,
+    color: "Red",
   },
 ];
 
@@ -43,16 +49,19 @@ export const backEnd = [
     id: 7,
     title: "Ruby",
     image: DiRuby,
+    color: "Red",
   },
   {
     id: 8,
     title: "Ruby on Rails",
     image: SiRails,
+    color: "Red",
   },
   {
     id: 9,
     title: "REST APIs",
     image: AiFillCloud,
+    color: "Red",
   },
 ];
 
@@ -61,15 +70,18 @@ export const others = [
     id: 10,
     title: "Git",
     image: AiFillGithub,
+    color: "Red",
   },
   {
     id: 11,
     title: "MySQL",
     image: GrMysql,
+    color: "Red",
   },
   {
     id: 12,
     title: "Heroku",
     image: GrHeroku,
+    color: "Red",
   },
 ];
